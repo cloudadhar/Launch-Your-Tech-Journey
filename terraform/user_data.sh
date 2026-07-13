5ade2af93b411a2ba2cc536c151dd518e843a430
gangadharure <40225657+gangadharure@users.noreply.github.com>
2026-07-13 12:43:09 +0530

    update

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -18,7 +18,7 @@ cat <<'HTML' | sudo tee /var/www/html/index.html
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Launch Your Tech Journey</title>
+  <title>Launch Your Tech Journey 2026</title>
   <style>
     * { box-sizing: border-box; }
     body {
